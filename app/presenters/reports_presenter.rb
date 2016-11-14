2870191124cfdd044106799c6ea4367aa83e1074
Anna Weisbrodt <aweisbro@gmail.com>
2016-11-13 17:33:13 -0700

    daily totals report table

--- app/presenters/reports_presenter.rb
+++ app/presenters/reports_presenter.rb
@@ -8,10 +8,19 @@ class ReportsPresenter
     Entry.where(date: date)
   end
 
+  def total_time_day
+    entries_day.sum(:value)
+  end
+
+  def categories_times_day
+    categories = entries_day.pluck(:category_id).uniq
+    totals = entries_day.group(:category_id).sum(:value)
+  end
+
   private
 
   def date
     @date
   end
-  
+
 end
--- spec/features/user_view_reports_spec.rb
+++ spec/features/user_view_reports_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 feature "User views reports" do
   scenario "user views daily report" do
     user = create(:user)
-    create_list(:entry, 5, user: user, date: Date.today)
+    category_1 = create(:category, name: "Rails")
+    category_2 = create(:category, name: "Java")
+
+    create_list(:entry, 2, user: user, category: category_1,
+                value: 30, date: Date.today)
+    create_list(:entry, 3, user: user, category: category_2,
+                value: 30, date: Date.today)
 
     sign_in(user)
 
@@ -16,6 +22,12 @@ feature "User views reports" do
     within(".daily-report") do
       expect(page).to have_content("task1")
       expect(page).to have_content("task5")
+    end
+
+    within(".daily-totals") do
+      expect(page).to have_content("Total 150")
+      expect(page).to have_content("Java 90")
+      expect(page).to have_content("Rails 60")
     end
 
   end
